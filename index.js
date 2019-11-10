5b1a2967fce94d8f11383a7ac3773656ea965a0f
Atheer Alaghbari <atheeralaghbari61@gmail.com>
2019-11-10 19:40:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function mapToNegativize(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(-1 * src[i]) // Unique work
+  }
+  return r
+}
+ 
+function mapToNoChange(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i]) // Unique work
+  }
+  return r
+}
+ 
+function mapToDouble(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(2 * src[i]) // Unique work
+  }
+  return r
+}
+ 
+function mapToSquare(src) {
+  let r = []
+  for (let i = 0; i < src.length; i++ ) {
+    r.push(src[i] * src[i]) // Unique work
+  }
+  return r
+}
+
